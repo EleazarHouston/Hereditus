5a90d8eda0de7c3f40c773d2c0d9adf76ae65a2f
EleazarHouston <habnef3@live.com>
2023-10-08 01:38:35 +0000

    Added test slash command

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,8 @@
+from email import message
 import discord
 from discord import app_commands
 
+guildID = input("GuildID: ")
 
 intents = discord.Intents.default()
 
@@ -9,6 +11,20 @@ intents.message_content = True
 client = discord.Client(intents = intents)
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "test", description = "Test command")
-async def command1(interaction):
+@tree.command(guild=discord.Object(id=guildID))
+async def command1(interaction: discord.Interaction):
     await interaction.response.send_message("Test message")
+    
+@client.event
+async def on_ready():
+    print(f"Bot {client.user.name} is ready") 
+
+@app_commands.command()
+async def slash(interaction: discord.Interaction, number: int, string: str):
+    await interaction.response.send_message(f'{number=} {string=}', ephemeral=True)
+
+# Can also specify a guild here, but this example chooses not to.
+tree.add_command(slash)
+
+token = input("Token: ")
+client.run(token)
