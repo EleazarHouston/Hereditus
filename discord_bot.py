0a9ff522f6b2c6864811e560cbb08bc4ca0ead29
EleazarHouston <habnef3@live.com>
2023-10-27 14:50:02 +0000

    Overhauled discord integration

--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,9 @@
 from email import message
 import discord
 from discord import app_commands
-import torb
+from player import Player
 import ast
-
+import traceback
 guildID = input("GuildID: ")
 
 intents = discord.Intents.default()
@@ -17,67 +17,101 @@ tree = app_commands.CommandTree(client)
 async def command1(interaction: discord.Interaction):
     await interaction.response.send_message("Everyone can see this!")
     await interaction.followup.send("Only you can see this!",ephemeral=True)
+    return
+
+@tree.command(guild=discord.Object(id=guildID))
+async def new_game(interaction: discord.Interaction):
+    from simulator import Simulator
+    a = Simulator(Simulator._next_SID)
+    a.new_evolution_engine()
+    await interaction.response.send_message(f"New game available: Game ID: {a.SID}\nRun /join_game to join the game, then wait for the game to start.")
+    return
+
+@tree.command(guild=discord.Object(id=guildID))
+async def start_game(interaction: discord.Interaction, sid: int, num_torbs: int):
+    from simulator import Simulator
+    try:
+        Simulator._instances[sid].init_all_colonies(num_torbs)
+    except KeyError:
+        await interaction.response.send_message(f"Command Invalid: Invalid Game ID")
+        return
+    await interaction.response.send_message("Colonies initialzed.\nGame has started\nPlayer commands available:\n/colony_info\n/breed")
+    return
+
+@tree.command(guild=discord.Object(id=guildID))
+async def join_game(interaction: discord.Interaction, game_id: int, colony_name: str):
+    if len(colony_name) > 16:
+        await interaction.response.send_message("Invalid Colony Name: Name must be 16 characters or fewer")
+        return
+    elif not colony_name.isalnum():
+        await interaction.response.send_message("Invalid Colony Name: Name must have only letters and numbers")
+        return
     
-@tree.command(guild=discord.Object(id=guildID))
-async def generate(interaction: discord.Interaction, colony_id: int):
+    from simulator import Simulator
+    
     try:
-        if not isinstance(colony_id, int):
-            await interaction.response.send_message("Please enter an integer for colony_id")
+        if interaction.user.id in Player._instances:
+            await interaction.response.send_message("Command Invalid: You have already joined this game.")
             return
-
-        if not colony_id in torb.Colony._instances:
-            torb.Colony(colony_id, f"C{colony_id}", 0)
-            
-        
-        torb.Colony._instances[colony_id].init_gen_zero(8)
-        #torb.C0.init_gen_zero(8)
-        out = torb.Colony._instances[colony_id].out_torbs()
-        await interaction.response.send_message(f"{out}")
-        #await interaction.followup.send("Only you can see this!",ephemeral=True)
-    except Exception as e:
-        await interaction.response.send_message(f"An error occurred: {e}")
-
-@tree.command(guild=discord.Object(id=guildID))
-async def reset_colony(interaction: discord.Interaction, colony_id: int):
-    try:
-        if not isinstance(colony_id, int):
-            await interaction.response.send_message("Please enter an integer for colony_id")
-            return
-        if not colony_id in torb.Colony._instances:
-            await interaction.response.send_message(f"Error: Colony not found")
-            return
-        #torb.Colony._instances[colony_id]
-        C99 = torb.Colony(colony_id, f"C{colony_id}", 0)
-        torb.Colony._instances[colony_id].init_gen_zero(8)
-        out = torb.Colony._instances[colony_id].out_torbs()
-        await interaction.response.send_message(f"{out}")
-    except Exception as e:
-        await interaction.response.send_message(f"An error occurred: {e}")
-
-@tree.command(guild=discord.Object(id=guildID))
-async def breed(interaction: discord.Interaction, colony_id: int, pairs: str):
-    try:
-        if not isinstance(colony_id, int):
-            await interaction.response.send_message("Please enter an integer for colony_id")
-            return
-        if not colony_id in torb.Colony._instances:
-            await interaction.response.send_message(f"Error: Colony not found")
-            return
-        pairs = ast.literal_eval(pairs)
-        if isinstance(pairs, list) and all(isinstance(sublist, list) and all(isinstance(item, int) for item in sublist) for sublist in pairs):
-            torb.Colony._instances[colony_id].colony_reproduction(pairs)
-            out = torb.Colony._instances[colony_id].out_torbs()
-            await interaction.response.send_message(f"{out}")
         else:
-            await interaction.response.send_message(f"Error: Incorrectly formatted breeding pairs, use format [[0, 1], [2, 3]] to breed Torb 0 and 1 together and Torb 2 and 3 together")
+            a = Player(interaction.user.id)
+            
+        Simulator._instances[game_id].new_colony(colony_name, 0, interaction.user.id)
+        
+        await interaction.response.send_message("Colony generated! Welcome to the game!")
+    except KeyError:
+        await interaction.response.send_message(f"Command Invalid: Invalid Game ID")
+    return
+
+@tree.command(guild=discord.Object(id=guildID))
+async def breed(interaction: discord.Interaction, breeding_pairs: str):
+    player_instance = Player._instances[interaction.user.id]
+    player_instance.call_colony_reproduction(player_instance.colonies[0], breeding_pairs)
+    await interaction.response.send_message(f"Torbs bred. Run /colony_info to see torbs")
+    return
+
+@tree.command(guild=discord.Object(id=guildID))
+async def colony_info(interaction: discord.Interaction):
+    print("Trying dsicord command colony_info")
+    try:
+        player_instance = Player._instances[interaction.user.id]
+        print(f"{interaction.user.id}")
+        print(f"{player_instance.colony_count}")
+        from simulator import Simulator
+        if Simulator._instances[0].started == False:
+            await interaction.response.send_message("The game hasn't started yet.\nPlease wait for an admin to start the game.")
+            return
+        if player_instance.colonies:
+            first_colony_key = list(player_instance.colonies.keys())[0]
+            print(first_colony_key)
+            gen_strings = []
+            for i in range(0, player_instance.colonies[0].generations+1):
+                gen_strings.append(player_instance.view_torbs(player_instance.colonies[first_colony_key].CID, i))
+            #torb_string = player_instance.view_torbs(player_instance.colonies[first_colony_key])
+            for i, gen_string in enumerate(gen_strings):
+                if i == 0:
+                    await interaction.response.send_message(f"{gen_strings[0]}")
+                else:
+                    await interaction.followup.send(f"{gen_strings[i]}")
+        else:
+        # handle case where player has no colonies, e.g., send a message
+            await interaction.response.send_message("You have no colonies.\nIf you haven't joined the game yet, please run command /join_game")
+            return
+        
+        #player_instance.view_torbs(player_instance.colonies[1])
     except Exception as e:
-        await interaction.response.send_message(f"An error occurred: {e}")
+        error_type = type(e).__name__
+        error_traceback = traceback.format_exc()
+        print(error_traceback)  # or save it to a log file
+        await interaction.response.send_message(f"Error ({error_type}): {e}")
+    return
 
 @client.event
 async def on_ready():
     await tree.sync()
     await tree.sync(guild=discord.Object(id=guildID))
     print(f"Bot {client.user.name} is ready") 
+    return
 
 @app_commands.command()
 async def slash(interaction: discord.Interaction, number: int, string: str):
