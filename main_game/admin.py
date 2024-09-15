1a8d51d27544896d334ce08e78ea921366666613
EleazarHouston <habnef3@live.com>
2024-09-15 01:11:24 +0000

    Added army and armytorb to admin screen

--- main_game/admin.py
+++ main_game/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Torb, Colony, Game, EvolutionEngine, StoryText
+from .models import Torb, Colony, Game, EvolutionEngine, StoryText, Army, ArmyTorb
 
 admin.site.register(Torb)
 admin.site.register(Colony)
 admin.site.register(Game)
 admin.site.register(EvolutionEngine)
 admin.site.register(StoryText)
+admin.site.register(Army)
+admin.site.register(ArmyTorb)
