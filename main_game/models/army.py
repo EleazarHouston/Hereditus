c72d45dff4bdd7609a8efce22d943c74214aa191
EleazarHouston <habnef3@live.com>
2024-10-13 13:15:43 +0000

    attack, scout targets and morale in Army class

--- main_game/models/army.py
+++ main_game/models/army.py
@@ -13,18 +13,19 @@ class Army(models.Model):
     colony = models.OneToOneField('main_game.Colony', on_delete=models.CASCADE, related_name='army_instance')
     scout_target = models.ForeignKey('colony', blank=True, null=True, on_delete=models.SET_NULL, related_name='scouting_armies')
     attack_target= models.ForeignKey('colony', blank=True, null=True, on_delete=models.SET_NULL, related_name='attacking_armies')
+    morale = models.IntegerField(default=100)
     
     @property
     def army_health(self):
-        return sum(army_torb.torb.hp for army_torb in self.army_torbs.all())
+        return round(sum(army_torb.torb.hp for army_torb in self.army_torbs.all()),0)
     
     @property
     def army_power(self):
-        return sum(army_torb.power for army_torb in self.army_torbs.all())
+        return round(sum(army_torb.power for army_torb in self.army_torbs.all()),2)
     
     @property
     def army_resilience(self):
-        return sum(army_torb.resilience for army_torb in self.army_torbs.all())
+        return round(sum(army_torb.resilience for army_torb in self.army_torbs.all()),2)
     
     def __str__(self):
         return f"Army of {self.colony.name}"
@@ -32,6 +33,7 @@ class Army(models.Model):
     def new_round(self):
         self.train_soldiers()
         self.scout_colony()
+        self.attack_colony()
         self.purge_soldiers()
 
     def train_soldiers(self):
@@ -54,29 +56,39 @@ class Army(models.Model):
             
     def set_scout_target(self, scout_target):
         from .colony import Colony
+        if not scout_target:
+            self.scout_target = None
+            self.save()
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="scout",
+                story_text=f"Our scouting plans have been canceled.",
+                timestamp=Now())
+            return
+        
         scout_target_colony = Colony.objects.get(id=scout_target)
-        logger.debug(f"{self.colony.name}: Set scout_target as {scout_target_colony.name}")
+        logger.debug(f"{self.colony.name}: Set scout_target as {scout_target_colony}")
         if scout_target_colony == self.colony:
             StoryText.objects.create(
                 colony=self.colony,
-                story_text_type="system",
+                story_text_type="scout",
                 story_text=f"We likely won't get any new information trying to scout ourselves.",
                 timestamp=Now())
             return
         
         if self.scout_target and self.scout_target in self.colony.discovered_colonies.all():
             StoryText.objects.create(colony=self.colony,
-                                     story_text_type="system",
+                                     story_text_type="scout",
                                      story_text=f"Our new scout target is {scout_target_colony.name}.",
                                      timestamp=Now())
         elif self.scout_target in self.colony.discovered_colonies.all():
             StoryText.objects.create(colony=self,
-                                     story_text_type="system",
+                                     story_text_type="scout",
                                      story_text=f"Our scout target is {scout_target_colony.name}.",
                                      timestamp=Now())
         else:
             StoryText.objects.create(colony=self.colony,
-                                     story_text_type="system",
+                                     story_text_type="scout",
                                      story_text=f"Our scouts will go out and learn about the as-yet-unknown colony.",
                                      timestamp=Now())
         self.scout_target = scout_target_colony
@@ -89,14 +101,19 @@ class Army(models.Model):
             return False
         
         if self.colony.num_soldiers == 0:
-            self._scout_no_soldiers()
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="scout",
+                story_text=f"You ordered your Torbs to scout, but without training all they found was {self.colony.name}.",
+                timestamp=Now(),
+                )
             return False
 
         if colony_to_scout.num_soldiers == 0:
             self._scout_undefended_colony(colony_to_scout)
             return True
         
-        random_enemy_torb_power = self._scout_random_enemy_power(colony_to_scout)
+        random_enemy_torb_power = random.choice([soldier.power for soldier in colony_to_scout.army.army_torbs.all()])
         random_ally_soldier = random.choice([soldier for soldier in self.army_torbs.all()])
         random_ally_torb_resilience = random_ally_soldier.resilience
 
@@ -112,15 +129,165 @@ class Army(models.Model):
         self.scout_target = None
         self.save()
         return False
+    
+    # DRY, definitely a better way to do this, repeats a lot of set_scout_target
+    def set_attack_target(self, attack_target):
+        from .colony import Colony
+        if not attack_target:
+            self.attack_target = None
+            self.save()
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"Our attack plans have been canceled.",
+                timestamp=Now())
+            return
+        attack_target_colony = Colony.objects.get(id=attack_target)
+        logger.debug(f"{self.colony.name}: Set attack_target as {attack_target_colony}")
+        if attack_target_colony == self.colony:
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"Our soldiers will never agree to attack their own colony!",
+                timestamp=Now())
+            return
+        if attack_target_colony not in self.colony.discovered_colonies.all():
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"We can't send our soldiers out without knowing where they're going!",
+                timestamp=Now())
+            return
+        
+        if self.attack_target:
+            StoryText.objects.create(colony=self.colony,
+                                     story_text_type="system",
+                                     story_text=f"Our new attack target is {attack_target_colony.name}.",
+                                     timestamp=Now())
+        else:
+            StoryText.objects.create(colony=self.colony,
+                                     story_text_type="system",
+                                     story_text=f"Our attack target is {attack_target_colony.name}.",
+                                     timestamp=Now())
+        self.attack_target = attack_target_colony
+        self.save()
+        
+    def attack_colony(self):
+        colony_to_attack = self.attack_target
+        if not colony_to_attack:
+            logger.debug(f"{self.colony}: No target colony to attack")
+            return False
+        logger.debug(f"{self.colony} is waging war against {colony_to_attack}")
+        if self.colony.num_soldiers == 0:
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"You ordered your Torbs to attack, but without training they just bumbled about.",
+                timestamp=Now(),
+                )
+            return False
+
+        won_fight = None
+        if colony_to_attack.num_soldiers == 0:
+            won_fight = True
+            StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"There was no army to defend your attack on {colony_to_attack.name}",
+                timestamp=Now(),
+                )
+        else:
+            won_fight = self.battle_army(enemy_army=colony_to_attack.army)
+        if won_fight:
+            self._attack_successful(colony_to_attack)
+        else:
+            self._attack_failed(colony_to_attack)
+        self.attack_target = None
+        self.save()
+        
+    def battle_army(self, enemy_army) -> bool:
+        ally_enough_morale = True
+        ally_enough_morale = self.morale > random.randrange(1, 50)
+        while self.colony.num_soldiers > 1 and enemy_army.colony.num_soldiers > 1 and ally_enough_morale:
+            ally_army_torb = self.army_torbs.all().order_by('?').first()
+            enemy_army_torb = enemy_army.army_torbs.all().order_by('?').first()
+            
+            self.torb_fight(ally_army_torb, enemy_army_torb)
+        
+        if enemy_army.colony.num_soldiers == 0:
+            return True
+        else:
+            return False
+    
+    def torb_fight(self, ally_army_torb, enemy_army_torb):
+        ally_army_torb_attack   = random.uniform(1, ally_army_torb.power)
+        ally_army_torb_defense  = random.uniform(1, ally_army_torb.resilience)
+        enemy_army_torb_attack  = random.uniform(1, enemy_army_torb.power)
+        enemy_army_torb_defense = random.uniform(1, enemy_army_torb.resilience)
+        
+        ally_army_torb_speed    = random.uniform(1, ally_army_torb.active_alleles['agility'])
+        enemy_army_torb_speed   = random.uniform(1, enemy_army_torb.active_alleles['agility'])
+        
+        if ally_army_torb_speed > enemy_army_torb_speed:
+            enemy_hp_adjust = round(min(0, enemy_army_torb_defense - ally_army_torb_attack),0)
+            enemy_army_torb.torb.adjust_hp(enemy_hp_adjust, context=f"defending against {self.colony.name}'s Army")
+            if enemy_army_torb.torb.is_alive:
+                ally_hp_adjust = round(min(0, ally_army_torb_defense - enemy_army_torb_attack),0)
+                ally_army_torb.torb.adjust_hp(ally_hp_adjust, context=f"in a battle against {enemy_army_torb.torb.colony.name}")
+        else: # Violates DRY
+            ally_hp_adjust = round(min(0, ally_army_torb_defense - enemy_army_torb_attack),0)
+            ally_army_torb.torb.adjust_hp(ally_hp_adjust, context=f"in a battle against {enemy_army_torb.torb.colony.name}")
+            if enemy_army_torb.torb.is_alive:
+                enemy_hp_adjust = round(min(0, enemy_army_torb_defense - ally_army_torb_attack),0)
+                enemy_army_torb.torb.adjust_hp(enemy_hp_adjust, context=f"defending against {self.colony.name}'s Army")
+        
+        if ally_hp_adjust > enemy_hp_adjust:
+            self.adjust_morale(1)
+            enemy_army_torb.army.adjust_morale(-1)
+        else:
+            self.adjust_morale(-1)
+            enemy_army_torb.army.adjust_morale(1)
+    
+    def adjust_morale(self, adjust_amount):
+        self.morale = max(0, min(100, self.morale + adjust_amount))
+        self.save()
+
+    def _attack_successful(self, enemy_colony):
+        stolen_food_amount = 0
+        if enemy_colony.food > 0:
+            stolen_food_amount = random.randrange(0, enemy_colony.food)
+            self.colony.adjust_food(stolen_food_amount)
+            enemy_colony.adjust_food(-1 * stolen_food_amount)
+        StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"Your army had a glorious victory over {enemy_colony.name} and plundered {stolen_food_amount} food.",
+                timestamp=Now(),
+                )
+        
+        StoryText.objects.create(
+                colony=enemy_colony,
+                story_text_type="combat",
+                story_text=f"We were ransacked by {self.colony.name} and they stole {stolen_food_amount} food.",
+                timestamp=Now(),
+                )
+    
+    def _attack_failed(self, enemy_colony):
+        StoryText.objects.create(
+                colony=self.colony,
+                story_text_type="combat",
+                story_text=f"Your army was defeated in a battle against {enemy_colony.name}.",
+                timestamp=Now(),
+                )
+        
+        StoryText.objects.create(
+                colony=enemy_colony,
+                story_text_type="combat",
+                story_text=f"We successfully repelled an attack from {self.colony.name}.",
+                timestamp=Now(),
+                )
 
     # Helper scout methods
-    def _scout_no_soldiers(self):
-        StoryText.objects.create(
-            colony=self.colony,
-            story_text_type="scout",
-            story_text=f"You ordered your Torbs to scout, but without training all they found was {self.colony.name}.",
-            timestamp=Now(),
-        )
 
     def _scout_undefended_colony(self, colony_to_scout):
         self.colony.discovered_colonies.add(colony_to_scout)
@@ -131,9 +298,6 @@ class Army(models.Model):
             story_text=f"Your scout found {colony_to_scout.name} and they reported that it appears undefended.",
             timestamp=Now(),
         )
-
-    def _scout_random_enemy_power(self, colony_to_scout):
-        return random.choice([soldier.power for soldier in colony_to_scout.army.army_torbs.all()])
 
     def _scout_successful(self, random_ally_soldier, colony_to_scout):
         self.colony.discovered_colonies.add(colony_to_scout)
@@ -201,7 +365,7 @@ class Army(models.Model):
 # Keep ArmyTorb in same file as Army
 class ArmyTorb(models.Model):
     army = models.ForeignKey('main_game.Army', on_delete=models.CASCADE, related_name='army_torbs')
-    torb = models.ForeignKey('main_game.Torb', on_delete=models.CASCADE)
+    torb = models.ForeignKey('main_game.Torb', on_delete=models.CASCADE, related_name='army_torb')
     active_alleles = models.JSONField(default=dict)
     
     @property
