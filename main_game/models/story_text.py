e672c87f0d66db8ccd0f4be78e93b490af40cc81
EleazarHouston <habnef3@live.com>
2024-10-20 19:29:55 +0000

    StoryText improvements, timestamp, game_round, is_new

--- main_game/models/story_text.py
+++ main_game/models/story_text.py
@@ -1,9 +1,27 @@
+import logging
+
 from django.db import models
+from django.utils.timezone import now
+
+logger = logging.getLogger('hereditus')
 
 class StoryText(models.Model):
     colony = models.ForeignKey('main_game.Colony', on_delete=models.CASCADE)
     story_text_type = models.CharField(max_length=32, default="default")
     story_text = models.CharField(max_length=1028, default="N/A")
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(default=now)
+    is_new = models.BooleanField(default=True)
+    game_round = models.IntegerField(default=-1)
     
     # TODO: Add StoryText to logger whenever created
+    
+    
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        if is_new:
+            self.game_round = self.colony.game.round_number
+        
+        super().save(*args, **kwargs)
+        
+        if is_new:
+            logger.debug(f"StoryText for Colony {self.colony.name} of type {self.story_text_type}: {self.story_text}")
