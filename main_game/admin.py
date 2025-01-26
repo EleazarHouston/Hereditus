83af097465f0119b892e768be6efb47a0233ca50
EleazarHouston <habnef3@live.com>
2025-01-26 05:09:17 +0000

    Better admin view, tabular info

--- main_game/admin.py
+++ main_game/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from .models import Torb, Colony, Game, EvolutionEngine, StoryText, Army, ArmyTorb, Player
 
-from .models import Torb, Colony, Game, EvolutionEngine, StoryText, Army, ArmyTorb
+class TorbAdmin(admin.ModelAdmin):
+    list_display = ('name', 'private_ID', 'colony', 'is_alive', 'hp', 'max_hp', 'action', 'action_desc')
 
-admin.site.register(Torb)
-admin.site.register(Colony)
-admin.site.register(Game)
-admin.site.register(EvolutionEngine)
+class ColonyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'player', 'game', 'food', 'ready')
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('description', 'starting_torbs', 'round_number', 'private', 'closed', 'max_colonies_per_player')
+
+class EvolutionEngineAdmin(admin.ModelAdmin):
+    list_display = ('game', 'random_gene_min', 'random_gene_max', 'mutation_chance', 'mutation_dev', 'alleles_per_gene')
+
+class ArmyAdmin(admin.ModelAdmin):
+    list_display = ('colony', 'scout_target', 'attack_target', 'morale')
+
+admin.site.register(Torb, TorbAdmin)
+admin.site.register(Colony, ColonyAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(EvolutionEngine, EvolutionEngineAdmin)
+admin.site.register(Army, ArmyAdmin)
 admin.site.register(StoryText)
-admin.site.register(Army)
 admin.site.register(ArmyTorb)
+admin.site.register(Player)
